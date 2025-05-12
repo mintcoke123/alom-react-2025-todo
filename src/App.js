f203cb508c447b721c8ee46cfc8d920a382926bd
강동현 <weetee1014@gmail.com>
2025-05-13 01:55:36 +0900

    feat: navigateToDongHyun 버튼 컴포넌트 구현

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 import Donghyun from "./assignments/donghyun";
+
+function NavigateToDongHyun() {
+  const navigate = useNavigate();
+
+  return (
+      <button onClick={() => navigate("/donghyun")}>동현 페이지로 이동</button>
+  );
+}
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<NavigateToDongHyun />} />
         <Route path="/donghyun" element={<Donghyun />} />
-        <Route
-          path="/"
-          element={<div>App.js와 example.js를 확인해주세요.</div>}
-        />
       </Routes>
     </Router>
   );
